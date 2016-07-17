0a47f21de6872b2b96808f0d9ac38aa2b572a4fd
rexwhitten <rwhitten@acrowire.com>
2016-07-17 10:21:14 -0400

    removing nodestation directory

--- nodestation/client/lib/index.js
+++
@@ -1,5 +0,0 @@
-﻿
-
-head.load('lib/jquery/js/jquery.min.js', function () { console.log("jquery loaded"); });
-
-head.load('lib/bootstrap/js/bootstrap.min.js', function () { console.log("bootstrap loaded"); });
